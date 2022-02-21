efe2f0a097f07289b5c1ca623334c89f82653be5
annecho <choann@sheridancollege.ca>
2022-02-21 04:06:43 -0500

    XD ADD ADDRESS XD

--- src/student_win22/Student.java
+++ src/student_win22/Student.java
@@ -4,6 +4,7 @@ public class Student {
     
     private String studentId;
     private String studentName;
+    private String address;
     
     public Student(String studentId, String studentName) {
         this.studentId = studentId;
@@ -25,5 +26,14 @@ public class Student {
     public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
     
 }
