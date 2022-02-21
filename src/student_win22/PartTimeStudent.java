28800c0acf1a35df86443229f22707e5fa252465
annecho <choann@sheridancollege.ca>
2022-02-21 16:42:12 -0500

    XD ADD Part Time Student XD

---
+++ src/student_win22/PartTimeStudent.java
@@ -0,0 +1,15 @@
+package student_win22;
+
+public class PartTimeStudent extends Student{
+    private int numCourses;
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+
+}
