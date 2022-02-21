4fd6d214e7e3c103177ffa124edd43841b588744
annecho <choann@sheridancollege.ca>
2022-02-21 03:30:09 -0500

    XD REVERTING XD

--- src/student_win22/StudentList.java
+++ src/student_win22/StudentList.java
@@ -3,7 +3,7 @@ package student_win22;
 
 public class StudentList {
     public static void main (String[] args){
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[3];
         studentList[0] = new Student("s1", "Ronak");
         studentList[1] = new Student("s2", "John");
         studentList[2] = new Student("s3", "Amir");
